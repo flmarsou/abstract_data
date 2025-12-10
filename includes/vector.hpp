db4eef7c88aa140ecc9a1abe9c070640259d16e3
flmarsou <marsouinflorent12@gmail.com>
2025-12-10 14:46:50 +0100

    Add all vector methods/functions declarations

--- includes/vector.hpp
+++ includes/vector.hpp
@@ -24,6 +24,108 @@ namespace	ft
 			typedef std::ptrdiff_t											difference_type;
 			typedef std::size_t												size_type;
 
+			// ============================================================== //
+			//    Constructor & Destructor                                    //
+			// ============================================================== //
+
+			explicit vector(const allocator_type &alloc = allocator_type());
+			explicit vector(size_type n, const value_type &val = value_type(), const allocator_type &alloc = allocator_type());
+			template <class InputIterator>
+			vector(InputIterator first, InputIterator last, const allocator_type &alloc = allocator_type());
+			vector(const vector &x);
+
+			vector	&operator=(const vector &x);
+
+			~vector();
+
+			// ============================================================== //
+			//    Iterators                                                   //
+			// ============================================================== //
+
+			iterator				begin();
+			const_iterator			begin() const;
+			iterator				end();
+			const_iterator			end() const;
+			reverse_iterator		rbegin();
+			const_reverse_iterator	rbegin() const;
+			reverse_iterator		rend();
+			const_reverse_iterator	rend() const;
+
+			// ============================================================== //
+			//    Capacity                                                    //
+			// ============================================================== //
+
+			size_type	size() const;
+			size_type	max_size() const;
+			void		resize(size_type n, value_type val = value_type());
+			size_type	capacity() const;
+			bool		empty() const;
+			void		reserve(size_type n);
+
+			// ============================================================== //
+			//    Element access                                              //
+			// ============================================================== //
+
+			reference			operator[](size_type n);
+			const_reference		operator[](size_type n) const;
+			reference			at(size_type n);
+			const_reference		at(size_type n) const;
+			reference			front();
+			const_reference		front() const;
+			reference			back();
+			const_reference		back() const;
+			value_type			*data();
+			const value_type	*data() const;
+
+			// ============================================================== //
+			//    Modifiers                                                   //
+			// ============================================================== //
+
+			template <class InputIterator>
+			void		assign(InputIterator first, InputIterator last);
+			void		assign(size_type n, const value_type &val);
+			void		push_back(const value_type &val);
+			void		pop_back();
+			iterator	insert(iterator position, const value_type &val);
+			void		insert(iterator position, size_type n, const value_type &val);
+			template <class InputIterator>
+			void		insert(iterator position, InputIterator first, InputIterator last);
+			iterator	erase(iterator position);
+			iterator	erase(iterator first, iterator last);
+			void		swap(vector &x);
+			void		clear();
+
+			// ============================================================== //
+			//    Allocator                                                   //
+			// ============================================================== //
+
+			allocator_type	get_allocator() const;
+
+			// ============================================================== //
+			//    Non-member function overloads                               //
+			// ============================================================== //
+
+			template <class U, class A>
+			friend bool	operator==(const vector<U, A> &lhs, const vector<U, A> &rhs);
+
+			template <class U, class A>
+			friend bool	operator!=(const vector<U, A> &lhs, const vector<U, A> &rhs);
+
+			template <class U, class A>
+			friend bool	operator<(const vector<U, A> &lhs, const vector<U, A> &rhs);
+
+			template <class U, class A>
+			friend bool	operator<=(const vector<U, A> &lhs, const vector<U, A> &rhs);
+
+			template <class U, class A>
+			friend bool	operator>(const vector<U, A> &lhs, const vector<U, A> &rhs);
+
+			template <class U, class A>
+			friend bool	operator>=(const vector<U, A> &lhs, const vector<U, A> &rhs);
+
+			template <class U, class A>
+			friend void	swap(vector<U, A> &x, vector<U, A> &y);
+
 		private:
 			pointer			_data;
 			size_type		_size;
@@ -31,3 +133,5 @@ namespace	ft
 			allocator_type	_alloc;
 	};
 }
+
+# include "vector.tpp"
